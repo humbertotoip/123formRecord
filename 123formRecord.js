95ae7dae2176c1dff5b2ad2e398958dccec53ffb
Humberto Pereira <72404782+humbertotoip@users.noreply.github.com>
2022-08-03 07:53:28 -0300

    Update 123formRecord.js

--- 123formRecord.js
+++ 123formRecord.js
@@ -1,11 +1,11 @@
 console.clear()
 let grav = window.parent.postMessage('get-grav','*');
-console.log(grav);
+//console.log(grav);
 window.addEventListener("message", receiveMessage, false);
 function receiveMessage(event){
-	console.log(event);
+	//console.log(event);
    	let gravacao = event['data'];
-   	console.log(gravacao);
+   	//console.log(gravacao);
 	document.getElementById(gravacao['field']).value = gravacao['grav'];
-	console.log(document.getElementById(gravacao['field']).value);
+	console.log('LINK DA GRAVAÇÃO = '+document.getElementById(gravacao['field']).value);
 }
